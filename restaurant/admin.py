710f0620ef1302a8c97aa0046a689bf5b5b1bd8d
Juan Lowy <juanmontenegro58@gmail.com>
2025-03-10 22:33:37 -0500

    Configuración de base de datos y modelos de reserva y menus

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    ...
