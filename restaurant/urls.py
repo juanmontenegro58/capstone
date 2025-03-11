ec0f476fe6e9631fab2fb2ee355c897a2da200ae
Juan Lowy <juanmontenegro58@gmail.com>
2025-03-10 23:18:11 -0500

    Configurada las vistas para reservas

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import (
+    path,
+    include
+)
+from rest_framework.routers import DefaultRouter
 
 from .views import *
 
+router = DefaultRouter()
+router.register(r'tables', BookingViewSet)
+
 urlpatterns = [
     # path('', sayHello, name = 'sayHello'),
-    path('', index, name = 'index')
+    path('', index, name = 'index'),
+
+    path('menu/', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView
+)
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import (
+    IsAuthenticated
+)
+
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def sayHello(request):
@@ -7,3 +19,20 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(ListCreateAPIView):
+
+    queryset = Menu.objects.all()
+    http_method_names = ['get', 'post']
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+
+    queryset = Menu.objects.all()
+    http_method_names = ['get', 'put', 'delete']
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
